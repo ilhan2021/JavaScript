76eb5b2ddccd4f36807bececb0595524aac14c97
ilhan2021 <ilhanilhan.2021@gmail.com>
2022-12-20 02:05:44 +0300

    js

--- practice/Day-03/app-10.js
+++ practice/Day-03/app-10.js
@@ -1,2 +1,26 @@
-//Büyük sayının küçük sayıya bölümünden kalanlar toplamını, küçük sayı büyük sayıya
+//Büyük sayının küçük sayıya bölümünden kalanlar toplamını, küçük sayı büyük sayıya 
 //yetişene kadar birer birer artırarak bulalım.
+
+let sayi1El = document.querySelector("#sayi1");
+let sayi2El = document.querySelector("#sayi2");
+let mesaj = document.querySelector("#msg");
+document.querySelector("#hesapla").addEventListener("click",()=>{
+    let sayi1 = sayi1El.value;
+    let sayi2 = sayi2El.value;
+
+    if(sayi1>sayi2){
+        let temp = sayi1;
+        sayi1 = sayi2;
+        sayi2=temp;
+    }
+
+    let kalanlar=0;
+
+    while(sayi2>sayi1){
+        kalanlar+=(sayi2%sayi1);
+        sayi1++
+    }
+    mesaj.innerText = `Kalanlar Toplamı = ${kalanlar}`;
+    sayi1El.value ="";
+    sayi2El.value ="";
+})
